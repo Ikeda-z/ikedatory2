b2856a0a1fd2bf2d6e91a9edcd8a6729f884da6e
Ikeda-z <chi.yasai55@icloud.com>
2024-07-31 23:08:04 +0900

    第５回演習課題

--- src/main/java/catstech/studentmanagement/StudentmanagementApplication.java
+++ src/main/java/catstech/studentmanagement/StudentmanagementApplication.java
@@ -1,20 +1,48 @@
 package catstech.studentmanagement;
 
+import ch.qos.logback.core.util.StringUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentmanagementApplication {
 
+	private String name = "Kitada chise";
+	private String age = "20";
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentmanagementApplication.class, args);
-	}
-	@GetMapping("/hello")
-	public String hello(){
-		return "Hello World";
 
 	}
+
+	@GetMapping("/studentInfo")
+	public String getStudentInfo() {
+		return name + " " + age + "歳";
+	}
+
+	@PostMapping("/studentInfo")
+	public void setstudentInfo(String name, String age) {
+		this.name = name;
+		this.age =age;
+
+	}
+	@PostMapping("/studentname")
+	public void updateStudentName(String name){
+		this.name = name;
+
+	}
+
 }
+
+
+
+
+
+
+
