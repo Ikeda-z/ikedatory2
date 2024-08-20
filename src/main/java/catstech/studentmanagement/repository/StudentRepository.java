efba22c4fb9b0d4858f630740d9bd61bf24ae0d0
Ikeda-z <chi.yasai55@icloud.com>
2024-08-20 15:17:12 +0900

    30代受講者とJavaコース受講者を全件表示してから、抽出するコードです。

--- src/main/java/catstech/studentmanagement/repository/StudentRepository.java
+++ src/main/java/catstech/studentmanagement/repository/StudentRepository.java
@@ -28,3 +28,5 @@ public interface StudentRepository {
   List<StudentsCourse> searchStudentsCourses();
 
 }
+
+
--- src/main/java/catstech/studentmanagement/service/StudentService.java
+++ src/main/java/catstech/studentmanagement/service/StudentService.java
@@ -3,7 +3,9 @@ package catstech.studentmanagement.service;
 import catstech.studentmanagement.data.Student;
 import catstech.studentmanagement.data.StudentsCourse;
 import catstech.studentmanagement.repository.StudentRepository;
+import java.util.ArrayList;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,16 +15,32 @@ public class StudentService {
   private StudentRepository repository;
 
   @Autowired
-  StudentService(StudentRepository repository) {
+  public StudentService(StudentRepository repository) {
     this.repository = repository;
   }
 
-  public List<Student> searchStudentList(){
-    return repository.search();
+  public List<Student> searchStudentList() {
+    List<Student> allStudents = repository.search();
+    List<Student> studentsIn30s = new ArrayList<>();
+    for (Student student : allStudents) {
+      int age = student.getAge();
+      if (age >= 30 && age <= 39) {
+        studentsIn30s.add(student);
+      }
+    }
+    return studentsIn30s;
   }
 
-  public List<StudentsCourse> searchStudentsCourseList(){
-    return repository.searchStudentsCourses();
+  public List<StudentsCourse> searchStudentsCourseList() {
+    List<StudentsCourse> allCourses = repository.searchStudentsCourses();
+    List<StudentsCourse> javaCourses = new ArrayList<>();
+    for (StudentsCourse courses : allCourses) {
+      if ("javaスタンダード".equals(courses.getCourse())) {
+        javaCourses.add(courses);
+      }
+    }
+    return javaCourses;
   }
 }
+
 
